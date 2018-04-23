d8c0e2a837464f088ff4e70284fcd6741808f837
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-04-23 16:52:18 +0300

    Increase font name buffer size

--- qt/qsciscintillabase.cpp
+++ qt/qsciscintillabase.cpp
@@ -1,19 +1,19 @@
 // This module implements the "official" low-level API.
 //
 // Copyright (c) 2018 Riverbank Computing Limited <info@riverbankcomputing.com>
-// 
+//
 // This file is part of QScintilla.
-// 
+//
 // This file may be used under the terms of the GNU General Public License
 // version 3.0 as published by the Free Software Foundation and appearing in
 // the file LICENSE included in the packaging of this file.  Please review the
 // following information to ensure the GNU General Public License version 3.0
 // requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-// 
+//
 // If you do not wish to use this file under the terms of the GPL version 3.0
 // then you may purchase a commercial license.  For more information contact
 // info@riverbankcomputing.com.
-// 
+//
 // This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
 // WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
@@ -181,6 +181,10 @@ long QsciScintillaBase::SendScintilla(unsigned int msg, unsigned long wParam,
     return sci->WndProc(msg, wParam, reinterpret_cast<sptr_t>(lParam));
 }
 
+sptr_t QsciScintillaBase::SendScintilla(unsigned int msg, uptr_t wParam, sptr_t lParam) const
+{
+    return sci->WndProc(msg, wParam, lParam);
+}
 
 // Overloaded message send.
 long QsciScintillaBase::SendScintilla(unsigned int msg, unsigned long wParam,
